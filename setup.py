8e0a498c2cac32043a8e1aaaca08b82433c03f42
Akaash Kamdar <kamdarakaash@gmail.com>
2024-07-02 11:37:05 -0400

    setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='BookLover',
+      version='0.1',
+      url='http://github.com/akaashkamdar/DS5100-booklover',
+      author='Akaash Kamdar',
+      author_email='ak2znr@virginia.edu',
+      license='MIT',
+      packages=['booklover', 'booklover_test']
+      )
